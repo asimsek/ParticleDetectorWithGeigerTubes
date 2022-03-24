d59a9f02ff072a901da9e80b6b59db8f9e4a6212
Ali Eren Simsek <ali.eren.simsek@cern.ch>
2022-03-24 18:11:29 +0300

    Data Acquisition Script - Raspberry Pi and MCP3008

---
+++ dataAcquisition.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import time
+import datetime as dt
+import matplotlib.pyplot as plt
+from MCP3008 import MCP3008
+from drawnow import *
+from scipy.signal import find_peaks, savgol_filter
+
+
+raw_adc = MCP3008()
+x, y = list(), list()
+
+ymin = -0.1
+ymax = 5.0
+xMax = 100
+voltageThreshold = 1.5 ## Volt
+
+
+
+plt.ion()
+fig=plt.figure(figsize=(16,9), dpi=60)
+def plotting():
+	plt.title('Osciloscope')
+	plt.grid(True)
+	plt.ylabel('Voltage')
+	plt.ylim(ymin, ymax)
+	plt.plot(x, y, '.b-', label='channel #0')
+	plt.legend(loc='lower right')
+	plt.show()
+	plt.pause(0.0001)
+
+
+
+fooVoltArr, fooDateArr = list(), list()
+def discriminator(voltage, signalDate):
+	try:
+		#if voltage < voltageThreshold and not fooVoltArr: voltage = 0.0; x.append(signalDate); y.append(voltage)
+		if voltage > voltageThreshold or fooVoltArr: fooVoltArr.append(voltage); fooDateArr.append(signalDate)
+		if voltage == 0.0 and fooVoltArr:
+			#### Smoothing the signal!
+			l = len(fooVoltArr)
+			if (l % 2) == 0: l-=1
+			y_filtered = savgol_filter(fooVoltArr, l, 5)
+			####
+
+			#### Always show the signal at the middle of the histogram!
+			for i in range(0, int(xMax/2)): x.append(i); y.append(0.0)
+			for j in range(0, len(fooVoltArr)): x.append(int(xMax/2)+j); y.append(y_filtered[j])
+			for k in range(int(xMax/2)+len(fooVoltArr)+1, xMax): x.append(k); y.append(0.0)
+			####
+
+			#### Plotting and clearing lists & figure!
+			plotting()
+			del fooVoltArr[:]
+			del fooDateArr[:]
+			del x[:]
+			del y[:]
+			fig.clf()
+	except:
+		pass
+	
+	
+
+def dataTake(ch=0):
+	os.system('mkdir -p logs/')
+	dateNow = dt.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+	print ("Start Date: %s" % (dateNow))
+	outTextFile = 'logs/log_%s.txt' % (dateNow)
+	print ("Data log File: %s" % (outTextFile))
+	
+	##### Start Data Taking
+	with open(outTextFile, 'w') as f:
+		dateC = -1
+		while True:
+			dateC += 1
+			valueC = raw_adc.read( channel = ch ) / 1023.0 * 3.3
+			
+			### Signal-BG Discrimination
+			discriminator(voltage = valueC, signalDate = dateC)
+
+
+if __name__== "__main__":
+	dataTake()
